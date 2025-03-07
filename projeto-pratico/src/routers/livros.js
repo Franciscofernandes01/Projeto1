f544c3705b55d6d7f3c97d95485c8271dc229f78
francisco fernandes <157524425+Franciscofernandes01@users.noreply.github.com>
2025-03-07 19:20:44 -0300

    Update livros.js

--- projeto-pratico/src/routers/livros.js
+++ projeto-pratico/src/routers/livros.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (require, response)=>{
+router.get('./src/routers/livros', (require, response)=>{
     response.json([
        {id:'1', title:'O senhor dos anéis', author:'J.R.R. Tolkien'},
        {id:'2', title:'1984', author:'George Orwell'},
